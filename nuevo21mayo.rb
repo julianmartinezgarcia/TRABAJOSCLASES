2a7658cff47024408100e7a9d7b4fb5447d33471
julianmartinezgarcia <julianmartinezgarcia@users.noreply.github.com>
2015-05-23 02:40:22 -0500

    Delete nuevo21mayo.rb

--- nuevo21mayo.rb
+++
@@ -1,7 +0,0 @@
-puts "deme su nombre, parce"
-nombre = gets.chomp
-puts "ciudad"
-ciudad = gets.chomp
-puts "fecha de nacimiento"
-fecha_de_nacimiento = gets.chomp
-puts "Hola, #{nombre}. Has nacido en #{ciudad}, el día #{fecha_de_nacimiento}"
